11f6071724af4a69395d0d4c988df76327d5afb6
Hector <vv96rca@gmail.com>
2021-12-21 16:56:24 +0530

    resolve linting errors

--- src/person.rb
+++ src/person.rb
@@ -1,24 +1,21 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
 
-class Person 
-    attr_accessor :name, :age
-    attr_reader :id
-
-def initialize (name = "Unknown",age, parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..10.000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
 
-end
+  private
 
+  def age_of?
+    @age >= 18
+  end
 
-private
-def is_age_of? 
-    @age >= 18 
-end
-
-def can_use_services?
-is_age_of or @parent_permission 
-end
-
+  def can_use_services?
+    age_of or @parent_permission
+  end
 end
--- src/techer.rb
+++ src/techer.rb
@@ -1,14 +1,12 @@
 require_relative 'person'
 
 class Techer < Person
+  def initialize(specialization, _classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def initialize(specialization,classroom, age, name = 'Unknown', parent_permission: true)
-        super(age, name, parent_permission)
-        @specialization = specialization
-      end
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
-
-
