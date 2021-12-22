9fce3db3441d69649c6e28c8388e3159f9086b96
Hector <vv96rca@gmail.com>
2021-12-22 16:39:32 +0530

    resolve linting issues

--- src/corrector.rb
+++ src/corrector.rb
@@ -1,10 +1,9 @@
 class Corrector
-    def initialize(name)
-        @name = name
-    end
-    def correct_name(name)
-        name.capitalize[1..10].strip
-    end
+  def initialize(name)
+    @name = name
+  end
 
-
+  def correct_name(name)
+    name.capitalize[1..10].strip
+  end
 end
