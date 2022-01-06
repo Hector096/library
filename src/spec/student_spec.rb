edd0e0d5178361b67d94be80bdd39c20bccf0017
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-06 15:23:56 +0000

    Add more negative tests

--- src/spec/student_spec.rb
+++ src/spec/student_spec.rb
@@ -1,13 +1,19 @@
 require_relative '../student'
 require_relative '../classroom'
+require_relative '../person'
 
 describe Student do
   context 'When providing student information' do
     classroom = Classroom.new('Microverse')
     person = Student.new(id: nil, classroom: classroom, age: 22, name: 'leo', parent_permission: true)
+    person_two = Student.new(id: nil, classroom: classroom, age: 16, name: 'leo', parent_permission: false)
 
     it 'creates a student person' do
       expect(person.name).to eq 'leo'
+    end
+
+    it 'creates a student without permission' do
+      expect(person_two.can_use_services?).to eq false
     end
 
     it 'adds student to classroom' do
