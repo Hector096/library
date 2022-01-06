b802f88a2385c62f96d56b91ec7978dc3d5b9031
Hector <vv96rca@gmail.com>
2022-01-06 18:33:52 +0530

    add person_spec test

---
+++ src/spec/person_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../person'
+
+
+describe Person do 
+    context "When passing a name" do
+        person = Person.new(id:nil,age: 22, name: "leo", parent_permission: true)
+       it "should return first letter capital" do 
+          expect(person.validate_name).to eq "Leo" 
+       end 
+         
+       it "should return true if he can you service or false" do 
+        expect(person.can_use_services?).to eq true 
+       end 
+
+       it "should add into rentals" do 
+        person.add_rentals(person)
+        expect(person.rentals.length).to eq 1 
+       end 
+    end 
+ end
