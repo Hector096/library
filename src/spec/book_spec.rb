2d35691a3dabfd804fbd7e6c75948c4b39c275a4
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-06 13:40:07 +0000

    Fix rubocop offenses

--- src/spec/book_spec.rb
+++ src/spec/book_spec.rb
@@ -1,22 +1,20 @@
 require_relative '../book'
 
+describe Book do
+  context 'It should create book and add rentals' do
+    book = Book.new('Harry Potter', 'JK Rowling')
+    it 'should create a neww book' do
+      expect(book.title).to eq 'Harry Potter'
+      expect(book.author).to eq 'JK Rowling'
+    end
 
-describe Book do 
-    context "It should create book and add rentals" do 
-        book = Book.new("Harry Potter","JK Rowling")
-       it "should create a neww book" do 
-          expect(book.title).to eq "Harry Potter" 
-          expect(book.author).to eq "JK Rowling"
-       end 
-         
-       it "should add new rentals" do 
-          book.add_rental(book)
-          expect(book.rentals.length).to eq 1
-       end 
+    it 'should add new rentals' do
+      book.add_rental(book)
+      expect(book.rentals.length).to eq 1
+    end
 
-       it "should return book in string" do 
-        expect(book.to_s).to eq  "Title: \"Harry Potter\", Author: JK Rowling"
-     end 
-       
-    end 
- end
+    it 'should return book in string' do
+      expect(book.to_s).to eq 'Title: "Harry Potter", Author: JK Rowling'
+    end
+  end
+end
--- src/spec/student_spec.rb
+++ src/spec/student_spec.rb
@@ -1,21 +1,21 @@
 require_relative '../student'
 require_relative '../classroom'
 
-describe Student do 
-    context "When providing student information" do
-        classroom = Classroom.new('Microverse')
-        person = Student.new(id:nil,classroom:classroom,age: 22, name: "leo", parent_permission: true)
-        
-        it 'creates a student person' do
-            expect(person.name).to eq "leo"
-        end
+describe Student do
+  context 'When providing student information' do
+    classroom = Classroom.new('Microverse')
+    person = Student.new(id: nil, classroom: classroom, age: 22, name: 'leo', parent_permission: true)
 
-        it 'adds student to classroom' do
-            expect(person.classroom).to eq classroom
-        end
-
-        it 'should print custom message' do
-            expect(person.play_hooky).to eq "¯\(ツ)/¯"
-        end
+    it 'creates a student person' do
+      expect(person.name).to eq 'leo'
     end
+
+    it 'adds student to classroom' do
+      expect(person.classroom).to eq classroom
+    end
+
+    it 'should print custom message' do
+      expect(person.play_hooky).to eq "¯\(ツ)/¯"
+    end
+  end
 end
